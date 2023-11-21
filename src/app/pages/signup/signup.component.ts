082db55d9d5eb0b63c5be08e105e36ca9bf50a5b
Simón Cherry <116177578+simoncherry9@users.noreply.github.com>
2023-11-21 10:30:04 -0300

    Manejo de errores al crear user

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,7 +1,8 @@
-import  Swal  from 'sweetalert2';
+import Swal from 'sweetalert2';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -11,26 +12,35 @@ import { Component, OnInit } from '@angular/core';
 export class SignupComponent implements OnInit {
 
   public user = {
-    username : '',
-    password : '',
-    nombre : '',
-    apellido : '',
-    email : '',
-    telefono : ''
-  }
+    username: '',
+    password: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: ''
+  };
 
-  constructor(private userService:UserService,private snack:MatSnackBar) { }
+  constructor(private userService: UserService, private snack: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
-  formSubmit(){
-    console.log(this.user);
-    if(this.user.username == '' || this.user.username == null){
-      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
-        duration : 3000,
-        verticalPosition : 'top',
-        horizontalPosition : 'right'
+  formSubmit() {
+    if (!this.user.username || !this.user.password || !this.user.nombre || !this.user.apellido || !this.user.email || !this.user.telefono) {
+      this.snack.open('Todos los campos son obligatorios', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
+      });
+      return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(this.user.email)) {
+      this.snack.open('El formato del correo electrónico no es válido', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
       });
       return;
     }
@@ -38,14 +48,22 @@ export class SignupComponent implements OnInit {
     this.userService.añadirUsuario(this.user).subscribe(
       (data) => {
         console.log(data);
-        Swal.fire('Usuario guardado','Usuario registrado con exito en el sistema','success');
-      },(error) => {
+        Swal.fire('Usuario guardado', 'Usuario registrado con éxito en el sistema', 'success');
+      },
+      (error: HttpErrorResponse) => {
         console.log(error);
-        this.snack.open('Ha ocurrido un error en el sistema !!','Aceptar',{
-          duration : 3000
-        });
+        if (error.status === 500 && error.error && error.error.message) {
+          if (error.error.message.includes('could not execute statement; SQL [n/a]; constraint [usuarios.email_UNIQUE]; nested exception is org.hibernate.exception.ConstraintViolationException: could not execute statement')) {
+            Swal.fire('Error', 'El correo electrónico ya está en uso. Por favor, utiliza otro correo electrónico.', 'error');
+          } else if (error.error.message.includes('El usuario ya esta presente')) {
+            Swal.fire('Error', 'Ya existe un usuario con ese nombre. Por favor, elige otro nombre de usuario.', 'error');
+          }
+        } else {
+          this.snack.open('Ha ocurrido un error en el sistema.', 'Aceptar', {
+            duration: 3000
+          });
+        }
       }
-    )
+    );
   }
-
 }
